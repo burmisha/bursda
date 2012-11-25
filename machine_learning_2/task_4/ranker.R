a312ed8c55f065fc28d0cdde79ed9e2e678c6029
burmisha <burmisha@gmail.com>
2012-11-25 21:44:36 +0400

    Simple pointwise - baseline

---
+++ machine_learning_2/task_4/ranker.R
@@ -0,0 +1,22 @@
+data = read.csv('learn_small.csv');
+test_data = read.csv('test.csv');
+
+# training linear score-function
+model = lm(label~.-qid-docid, data);
+
+# getting score-values of test documents
+scores = predict(model, test_data);
+
+test_data = test_data[,1:3];
+test_data$label = scores;
+
+# sorting docs for each query
+qids = unique(test_data$qid);
+for (doc_qid in qids) {
+  qid_docs = test_data[which(test_data$qid == doc_qid),];
+  qid_docs = qid_docs[order(qid_docs$label, decreasing=TRUE),];
+  test_data[which(test_data$qid == doc_qid),] = qid_docs;
+}
+test_data = test_data[,2:3];
+
+write.csv(test_data, "result.csv", row.names=FALSE, quote=FALSE);
