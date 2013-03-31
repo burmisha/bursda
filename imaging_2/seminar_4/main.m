f007569590f641b7ca053731ec99edabaed703e7
burmisha <burmisha@gmail.com>
2013-03-31 20:49:23 +0400

    simplify, add lena

--- imaging_2/seminar_4/main.m
+++ imaging_2/seminar_4/main.m
@@ -4,33 +4,21 @@ run('../vlfeat/toolbox/vl_setup')
 
 % im = imread('Zebra_Kruger.jpg');
 % im = imread('Zebra-Resimleri5-300x230.jpg');
-im = imread('91010177_large_MTS_Blaggiii1244376picture6.jpg');
+% im = imread('91010177_large_MTS_Blaggiii1244376picture6.jpg');
+im = imread('lena_std.tif');
 w = size(im,1);
 h = size(im,2);
-
-regionSize = 40 ;
-regularizer = 500 ;
+%%
+regionSize = 35 ;
+regularizer = 2;
 imlab = vl_xyz2lab(vl_rgb2xyz(im));
 segments = vl_slic(im2single(imlab), regionSize, regularizer);
-mask = -ones([w,h] + [2,2]);
-mask(2:(w+1),2:(h+1)) = segments;
-%%
-
-lines = ones(size(im))*255;
-for i=2:(w+1)
-    for j=2:(h+1)
-        if (mask(i,j) ~= mask(i+1,j)) || (mask(i,j) ~= mask(i-1,j)) || (mask(i,j) ~= mask(i,j+1)) || (mask(i,j) ~= mask(i,j-1))
-           lines(i-1,j-1,:) = [0, 0, 0];
-        end
-    end
-end
-
-
-size(segments)
-max(max(segments))
-min(min(segments))
-imshow(lines)
-
+m = -ones([w,h] + [2,2]);   m(2:(w+1),2:(h+1)) = segments;
+a = 2:(w+1);                b = 2:(h+1);
+border = (m(a,b) ~= m(a-1,b)) + (m(a,b) ~= m(a+1,b)) + (m(a,b) ~= m(a,b-1)) + (m(a,b) ~= m(a,b+1));
+border = repmat(border,[1 1 3]);
+% imshow(double(im) .* double(border == 0)/255);
+imshow(double(border == 0));
 %% 
 % idea: people change poiniins soon
 % robots give multiple deals
