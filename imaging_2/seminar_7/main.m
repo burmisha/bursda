7782fd607eb9563c6f437e8dd86a6a0a4d56fe32
burmisha <burmisha@gmail.com>
2013-04-21 21:25:39 +0400

    PCA

--- imaging_2/seminar_7/main.m
+++ imaging_2/seminar_7/main.m
@@ -8,26 +8,44 @@ NN_Idx = knnsearch(train_faces', test_faces');
 NN_Answers = train_labels(NN_Idx);
 NN_Quality = sum(NN_Answers == test_labels) / length(test_labels)
 
-%% PCA
+%% PCA http://blog.cordiner.net/2010/12/02/eigenfaces-face-recognition-matlab/
+mean_face = mean(train_faces, 2);                                           % find the mean image
+shifted_train = train_faces - repmat(mean_face, 1, size(train_faces,2));    % mean-shifted input images
+[evectors, score, evalues] = princomp(shifted_train');                      % calculate the ordered eigenvectors and eigenvalues
+features = evectors' * shifted_train;                                       % project the images into the subspace to generate the feature vectors
 
-[coeff, score, latent] = princomp(1*(train_faces));
-
-size(coeff)
-num_eigenfaces = 20;        k = ceil(sqrt(num_eigenfaces+1)); 
+%%
+size(score)
+show_eigenfaces = 20;        k = ceil(sqrt(show_eigenfaces+1)); 
 subplot(k, k, 1);
-m = mean(train_faces,2)
-imshow(vec2mat(m,32)'/256)
-for n = 1:num_eigenfaces
-    subplot(k, k, n+1);       imshow(vec2mat(score(:,n),32)'/512+0.5);
+imshow(vec2mat(mean_face,32)'/256)
+for n = 1:show_eigenfaces
+    subplot(k, k, n+1);       imshow(vec2mat(evectors(:,n),32)'*4+0.5);
 end
 
-proj = score' * test_faces;
-PA_Idx = knnsearch(coeff(:,4:end), proj(4:end,:)');
-PCA_Answers = train_labels(PA_Idx);
-PCA_Quality = sum(PCA_Answers == test_labels) / length(test_labels)
-
-%% Analyse image 17
-
-for i=[1,2,5,10,50,100]
-    imshow()
+%% Analyse images 17 and 191
+close all
+for k = [17, 191]
+    for i=[1,2,5,10,50,100]
+        imshow(vec2mat(mean_face + evectors(:,1:i) * (score(k, 1:i))',32)'/255)
+        % pause
+    end
 end
+
+%% calculate the similarity of the input to each training image
+ei = 10:100
+for e = ei
+    e
+    num_eigenfaces = 4:e;                                                     % only retain the top 'num_eigenfaces' eigenvectors
+    evectors_small = evectors(:, num_eigenfaces);
+    features_small = evectors_small' * shifted_train;   
+    feature_vec = evectors_small' * (test_faces - repmat(mean_face, 1, size(test_faces,2)));
+    for i=1:size(test_faces,2)
+        similarity_score = arrayfun(@(n) 1 / (1 + norm(features_small(:,n) - feature_vec(:,i))), 1:size(train_faces,2));
+        [~, match_ix] = max(similarity_score);
+        PCA_Idx(i) = match_ix;
+    end
+    PCA_Answers = train_labels(PCA_Idx);
+    PCA_Quality(e) = sum(PCA_Answers == test_labels) / length(test_labels);
+end
+plot(ei, PCA_Quality(ei))
