469e72e099dd17123a968bbb860ef3e4c379cb81
burmisha <burmisha@gmail.com>
2013-04-21 17:40:58 +0400

    prepare for sending

--- imaging_2/seminar_4/main.m
+++ imaging_2/seminar_4/main.m
@@ -3,21 +3,28 @@ run('../vlfeat/toolbox/vl_setup')
 % im = imread('Zebra_Kruger.jpg');
 % im = imread('Zebra-Resimleri5-300x230.jpg');
 im = imread('91010177_large_MTS_Blaggiii1244376picture6.jpg');
-% im = imread('lena_std.tif');
+regionSize = 32 ;
+regularizer = 40;
+
+%%
+clear all;
+run('../vlfeat/toolbox/vl_setup')
+im = imread('lena_std.tif');
+regionSize = 30 ;
+regularizer = 20;
+
+%%
 w = size(im,1);
 h = size(im,2);
 
-%%
-regionSize = 20 ;
-regularizer = 80;
 imlab = vl_xyz2lab(vl_rgb2xyz(im));
 segments = vl_slic(im2single(imlab), regionSize, regularizer)+1;
 m = -ones([w,h] + [2,2]);   m(2:(w+1),2:(h+1)) = segments;
 a = 2:(w+1);                b = 2:(h+1);
 border = (m(a,b) ~= m(a-1,b)) + (m(a,b) ~= m(a+1,b)) + (m(a,b) ~= m(a,b-1)) + (m(a,b) ~= m(a,b+1));
 border = repmat(border,[1 1 3]);
-% imshow(double(im) .* double(border == 0)/255);
-imshow(double(border == 0));
+imshow(double(im) .* double(border == 0)/255);
+% imshow(double(border == 0));
 
 %% 
 SegNum = max(max(segments));
