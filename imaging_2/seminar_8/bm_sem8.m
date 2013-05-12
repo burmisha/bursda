442b88aa511315df79bcc571f046db12113679ec
burmisha <burmisha@gmail.com>
2013-05-12 09:26:11 +0400

    add fast plot3

--- imaging_2/seminar_8/bm_sem8.m
+++ imaging_2/seminar_8/bm_sem8.m
@@ -1,3 +1,4 @@
+%% 
 clear all
 
 load lines.mat
@@ -11,8 +12,8 @@ for i=1:size(startpoints,2)
     line([x1; x2], [y1; y2],'Color',[0.2 0.2 0.2])
 end
 
-us2homo = @(x) [x(:); 1]
-homo2us = @(x) x(1:2)./x(3);
+us2homo = @(x) [x(:); 1];
+homo2us = @(x) x(1:(end-1),:)./(ones(size(x,1)-1,1)*x(end,:));
 
 H1 = [sqrt(3), -1, 1; 1 sqrt(3) 1; 0 0 2];
 H2 = [1 -1 1; 1 1 0; 0 0 1];
@@ -21,17 +22,12 @@ H4 = [sqrt(3), -1, 1; 1 sqrt(3) 1; 0.25 0.5 2];
 
 H=H1;
 for i=1:size(startpoints,2)
-    x1 = startpoints(1,i);
-    x2 = endpoints(1,i);
-    y1 = startpoints(2,i);
-    y2 = endpoints(2,i);
-    p1 = H*us2homo([x1; y1]);
-    p2 = H*us2homo([x2; y2]);
-    d1 = homo2us(p1);
-    d2 = homo2us(p2);
-    line([d1(1); d2(1)], [d1(2); d2(2)],'Color',[0.2 0.8 0]);
+    x1 = startpoints(1,i);      x2 = endpoints(1,i);
+    y1 = startpoints(2,i);      y2 = endpoints(2,i);
+    p1 = H*us2homo([x1; y1]);   p2 = H*us2homo([x2; y2]);
+    d1 = homo2us(p1);           d2 = homo2us(p2);
+    line([d1(1); d2(1)], [d1(2); d2(2)],'Color',[0 0.5 0.5]);
 end
-
 H=H2;
 for i=1:size(startpoints,2)
     x1 = startpoints(1,i);      x2 = endpoints(1,i);
@@ -57,3 +53,16 @@ for i=1:size(startpoints,2)
     line([d1(1); d2(1)], [d1(2); d2(2)],'Color',[1 0 0]);
 end
 axis equal
+
+%%
+clear all
+close all
+load 3Dto2D.mat
+whos
+
+us2homo = @(x) [x(:); 1];
+homo2us = @(x) x(1:(end-1),:)./(ones(size(x,1)-1,1)*x(end,:));
+P = homo2us(U);
+idx = 1:10:37000;
+plot3(P(1,idx),P(2,idx),P(3,idx),'.', 'markersize',1)
+
