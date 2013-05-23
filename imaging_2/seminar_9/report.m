658fd63b5039ed0b64c5f4de70478495dff0163a
burmisha <burmisha@gmail.com>
2013-05-23 17:22:22 +0400

    normalize

--- imaging_2/seminar_9/report.m
+++ imaging_2/seminar_9/report.m
@@ -1,2 +1,17 @@
 clear all
 load estimateF.mat
+
+us2homo = @(x) [x; ones(1, size(x,2))];
+homo2us = @(x) x(1:(end-1),:)./(ones(size(x,1)-1,1)*x(end,:));
+
+x1 = cell2mat(x(1));
+meaned_x1 = x1 - mean(x1,2)*ones(1,size(x1,2));
+t = meaned_x1(1:2,:)';
+N1 = diag([1./std(t) 1]);
+n1 = N1 * meaned_x1;
+
+x2 = cell2mat(x(2));
+meaned_x2 = x2 - mean(x2,2)*ones(1,size(x2,2));
+t = meaned_x2(1:2,:)';
+N2 = diag([1./std(t) 1]);
+n2 = N2 * meaned_x2;
