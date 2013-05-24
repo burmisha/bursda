461cfe9215f889bc2465966188108bb47fb2e330
burmisha <burmisha@gmail.com>
2013-05-24 13:55:43 +0400

    both algo

--- imaging_2/seminar_10/report.m
+++ imaging_2/seminar_10/report.m
@@ -8,23 +8,28 @@ disp_window = -30:30;
 h = size(im1,1);
 w = size(im1,2);
 
+sad.transformation = @(x) abs(x);
+sad.name = 'SAD';
+ssd.transformation = @(x) x.^2;
+ssd.name = 'SSD';
+
 cut = @(x, l, r) [zeros(1,min(max(1-l,0),r-l+1)), x(max(1,l):min(length(x),r)), zeros(1,max(r-length(x),0))];
-for m=1:30
-    m
-    mask = ones(m,1);
-    Disparity = zeros(h, w);
-    for r = 1:h
-        Dist = zeros(length(disp_window), w);
-        for j = 1:length(disp_window)
-            d = disp_window(j);
-            line_2 = cut(im2(r,:), 1+d, w+d);
-            delta = double(im1(r,:)) - double(line_2);
-            Dist(j,:) = conv(delta.^2, mask, 'same'); % SSD
-%             Dist(j,:) = conv(abs(delta), mask, 'same'); % SAD
+for algo = [sad ssd]
+    for m=1:50
+        m
+        mask = ones(m,1);
+        Disparity = zeros(h, w);
+        for r = 1:h
+            Dist = zeros(length(disp_window), w);
+            for j = 1:length(disp_window)
+                d = disp_window(j);
+                line_2 = cut(im2(r,:), 1+d, w+d);
+                delta = double(im1(r,:)) - double(line_2);
+                Dist(j,:) = conv(algo.transformation(delta), mask, 'same');
+            end
+            [~,d_idx] = min(Dist);
+            Disparity(r,:) = disp_window(d_idx);
         end
-        [~,d_idx] = min(Dist);
-        Disparity(r,:) = disp_window(d_idx);
+        imwrite((Disparity+30)*255/60, sprintf('%s_m%02d.gif', algo.name, m))
     end
-%     imshow(Disparity,[-30,30])
-    imwrite((Disparity+30)*255/60, sprintf('SSD_m%02d.gif', m))
 end
